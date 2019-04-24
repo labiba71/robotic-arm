1be6c31fac041eead424c5750e051bfe9601d528
labiba71 <labiba.ssp@gmail.com>
2019-04-25 00:06:33 +0600

    tenth commit

---
+++ SkeletonBasics-WPF/Window1.xaml.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            MainWindow dashboard = new MainWindow();
+            dashboard.Show();
+        }
+        bool closing = false;
+        const int skeletonCount = 6;
+        Skeleton[] allSkeletons = new Skeleton[skeletonCount];
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            kinectSensorChooser1.KinectSensorChanged += new DependencyPropertyChangedEventHandler(kinectSensorChooser1_KinectSensorChanged);
+
+        }
+
+        void kinectSensorChooser1_KinectSensorChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            KinectSensor old = (KinectSensor)e.OldValue;
+
+            StopKinect(old);
+
+            KinectSensor sensor = (KinectSensor)e.NewValue;
+
+            if (sensor == null)
+            {
+                return;
+            }
+
+            sensor.SkeletonStream.Enable();
+            sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+            sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+            sensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(sensor_AllFramesReady);
+
+            try
+            {
+                sensor.Start();
+            }
+            catch (System.IO.IOException)
+            {
+                kinectSensorChooser1.AppConflictOccurred();
+            }
+        }
+
+        void sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            if (closing)
+            {
+                return;
+            }
+
+            //Get a skeleton
+            Skeleton first=null;
+
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame()) // Open the Skeleton frame
+            {
+                if (skeletonFrame != null ) // check that a frame is available
+                {
+                    //skeletonFrame.CopySkeletonDataTo(this.skeletonData); // get the skeletal information in this frame
+                     first = GetFirstSkeleton(e);
+                     byte[] angles=GetVector(first);
+                    angleBox.Text = angles[1].ToString();
+                    angleBox.Text = angles[0].ToString();
+                }
+            }
+
+            if (first == null)
+            {
+                return;
+            }
+
+        }
+        Skeleton GetFirstSkeleton(AllFramesReadyEventArgs e)
+        {
+            using (SkeletonFrame skeletonFrameData = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrameData == null)
+                {
+                    return null;
+                }
+
+
+                skeletonFrameData.CopySkeletonDataTo(allSkeletons);
+
+                //get the first tracked skeleton
+                Skeleton first = (from s in allSkeletons
+                                  where s.TrackingState == SkeletonTrackingState.Tracked
+                                  select s).FirstOrDefault();
+
+                return first;
+
+            }
+        }
+
+        private void StopKinect(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                if (sensor.IsRunning)
+                {
+                    //stop sensor 
+                    sensor.Stop();
+
+                    //stop audio if not null
+                    if (sensor.AudioSource != null)
+                    {
+                        sensor.AudioSource.Stop();
+                    }
+
+
+                }
+            }
+        }
+
+        public double AngleBetweenTwoVectors(Vector3D vectorA, Vector3D vectorB)
+        {
+            double dotProduct;
+            vectorA.Normalize();
+            vectorB.Normalize();
+            dotProduct = Vector3D.DotProduct(vectorA, vectorB);
+
+            return (double)Math.Acos(dotProduct) / Math.PI * 180;
+        }
+
+        public byte[] GetVector(Skeleton skeleton)
+        {
+            Vector3D RightShoulder = new Vector3D(skeleton.Joints[JointType.ShoulderRight].Position.X, skeleton.Joints[JointType.ShoulderRight].Position.Y, skeleton.Joints[JointType.ShoulderRight].Position.Z);
+            Vector3D LeftShoulder = new Vector3D(skeleton.Joints[JointType.ShoulderLeft].Position.X, skeleton.Joints[JointType.ShoulderLeft].Position.Y, skeleton.Joints[JointType.ShoulderLeft].Position.Z);
+            Vector3D RightElbow = new Vector3D(skeleton.Joints[JointType.ElbowRight].Position.X, skeleton.Joints[JointType.ElbowRight].Position.Y, skeleton.Joints[JointType.ElbowRight].Position.Z);
+            Vector3D LeftElbow = new Vector3D(skeleton.Joints[JointType.ElbowLeft].Position.X, skeleton.Joints[JointType.ElbowLeft].Position.Y, skeleton.Joints[JointType.ElbowLeft].Position.Z);
+            Vector3D RightWrist = new Vector3D(skeleton.Joints[JointType.WristRight].Position.X, skeleton.Joints[JointType.WristRight].Position.Y, skeleton.Joints[JointType.WristRight].Position.Z);
+            Vector3D LeftWrist = new Vector3D(skeleton.Joints[JointType.WristLeft].Position.X, skeleton.Joints[JointType.WristLeft].Position.Y, skeleton.Joints[JointType.WristLeft].Position.Z);
+
+
+            double AngleRightElbow = AngleBetweenTwoVectors(RightElbow - RightShoulder, RightElbow - RightWrist);
+            double AngleLeftElbow = AngleBetweenTwoVectors(LeftElbow - LeftShoulder, LeftElbow - LeftWrist);
+
+            byte[] Angles = { Convert.ToByte(AngleRightElbow), Convert.ToByte(AngleLeftElbow) };
+            return Angles;
+        }
+
+
+    }
+}
---
+++ SkeletonBasics-WPF/com_connect.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics
+{
+    class com_connect
+    {
+        static SerialPort port;
+
+        public void start()
+        {
+            int baud;
+            string name;
+            Console.WriteLine("Welcome, enter parameters to begin");
+            Console.WriteLine(" ");
+            Console.WriteLine("Available ports:");
+            if (SerialPort.GetPortNames().Count() >= 0)
+            {
+                foreach (string p in SerialPort.GetPortNames())
+                {
+                    Console.WriteLine(p);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No Ports available, press any key to exit.");
+                Console.ReadLine();
+                // Quit
+                return;
+            }
+            /*Console.WriteLine("Port Name:");
+            name = Console.ReadLine();
+            Console.WriteLine(" ");
+            Console.WriteLine("Baud rate:");
+            baud = GetBaudRate();
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Beging Serial...");*/
+            BeginSerial(115200, "COM4");
+            port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            port.Open();
+            Console.WriteLine("Serial Started.");
+            Console.WriteLine(" ");
+            Console.WriteLine("Ctrl+C to exit program");
+            Console.WriteLine("Send:");
+
+           /* for (; ; )
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("> ");
+                port.WriteLine(Console.ReadLine());
+            }*/
+        }
+
+        public void sendtoport(String s) {
+
+            //if(s!=null)
+            port.WriteLine(s);
+        }
+
+        public void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            for (int i = 0; i < (10000 * port.BytesToRead) / port.BaudRate; i++)
+                ;       //Delay a bit for the serial to catch up
+            Console.Write(port.ReadExisting());
+            Console.WriteLine("");
+            Console.WriteLine("> ");
+        }
+
+        public void BeginSerial(int baud, string name)
+        {
+            port = new SerialPort(name, baud);
+        }
+
+        public int GetBaudRate()
+        {
+            try
+            {
+                return int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Invalid integer.  Please try again:");
+                return GetBaudRate();
+            }
+        }
+    }
+}
+
